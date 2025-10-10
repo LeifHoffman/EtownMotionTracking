a1d745e0802baf002d3a3242de183671806be81a
LeifHoffman <hoffmanl2@etown.edu>
2025-10-10 19:51:38 -0400

    Added leg tracking (needs refined)

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import mediapipe as mp
 # import libraries for console clearing
 import os
 import subprocess
+# import library for math calculation
+import math
 
 
 mp_pose = mp.solutions.pose
@@ -45,10 +47,23 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             if delay_counter < print_delay:
                 delay_counter += 1
             else:
+                # Get nose coords (please change later)
                 nose = [lm[0].x, lm[0].y]
+                # Get right leg coords
+                right_leg_upper = [lm[24].x, lm[24].y]
+                right_leg_mid = [lm[26].x, lm[26].y]
+                right_leg_lower = [lm[28].x, lm[28].y]
+                # Get left leg coords
+                left_leg_upper = [lm[23].x, lm[23].y]
+                left_leg_mid = [lm[25].x, lm[25].y]
+                left_leg_lower = [lm[27].x, lm[27].y]
+
                 if prev_point != None:
                     subprocess.call(cmd, shell=True)
                     print("Velocity X:", round(nose[0]-prev_point[0], 3), "\nVelocity Y:", round(-(nose[1]-prev_point[1]), 3))
+                    print("Right Leg Angle:", round(math.degrees(math.atan2(right_leg_lower[1]-right_leg_mid[1], right_leg_lower[0]-right_leg_mid[0]) - math.atan2(right_leg_upper[1]-right_leg_mid[1], right_leg_upper[0]-right_leg_mid[0])), 2))
+                    print("Left Leg Angle:", round(math.degrees(math.atan2(left_leg_lower[1]-left_leg_mid[1], left_leg_lower[0]-left_leg_mid[0]) - math.atan2(left_leg_upper[1]-left_leg_mid[1], left_leg_upper[0]-left_leg_mid[0])), 2))
+
                 # Set to prev_point
                 prev_point = nose
                 # reset delay counter
