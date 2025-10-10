90b64e323c90590781107cb8d1b155545a731229
LeifHoffman <hoffmanl2@etown.edu>
2025-10-10 19:18:10 -0400

    Added loose velocity tracking

--- test.py
+++ test.py
@@ -1,9 +1,24 @@
+# import libraries for video capture and pose estimation
 import cv2
 import mediapipe as mp
-import keyboard as kb
+# import libraries for console clearing
+import os
+import subprocess
+
 
 mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
+
+# counter variables for delay
+delay_counter = 0
+print_delay = 10
+
+# Track velocity of given point
+prev_point = None
+
+# set cmd to clear console later
+cmd = "cls" if os.name == "nt" else "clear"
+
 
 cap = cv2.VideoCapture(0)  # change to video file path if you prefer
 
@@ -24,12 +39,20 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                 mp_pose.POSE_CONNECTIONS)
 
             # Example: access landmark coordinates (normalized x,y,z)
-            # Nose = index 0, LEFT_ELBOW = 13, LEFT_KNEE = 25 (see MediaPipe docs)
+            # Nose = index 0 (see MediaPipe docs)
             lm = results.pose_landmarks.landmark
-            nose = (lm[0].x, lm[0].y, lm[0].z)
-            left_elbow = (lm[13].x, lm[13].y, lm[13].z)
-            left_knee = (lm[25].x, lm[25].y, lm[25].z)
-            print("nose:", nose, "L_elbow:", left_elbow, "L_knee:", left_knee)
+            #increment delay counter until delay reached
+            if delay_counter < print_delay:
+                delay_counter += 1
+            else:
+                nose = [lm[0].x, lm[0].y]
+                if prev_point != None:
+                    subprocess.call(cmd, shell=True)
+                    print("Velocity X:", nose[0]-prev_point[0], "\nVelocity Y:", -(nose[1]-prev_point[1]))
+                # Set to prev_point
+                prev_point = nose
+                # reset delay counter
+                delay_counter = 0
 
         cv2.imshow('Pose', image)
         if cv2.waitKey(1) & 0xFF == 27:
